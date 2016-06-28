3cfbf502f014dd96fa6e9f3e7d7e4caaee93cd72
Carsten Rattay <techadler@techadler.de>
2016-06-28 12:39:23 +0200

    - Added Collision Simulation for nodes  - Uses a QuadTree - Added Vector class - Added electricalForce simulation - Added Color class

--- graph.js
+++ graph.js
@@ -129,20 +129,6 @@ atc.base = atc.base || (function () {
   };
 })();
 
-function Node (t_id, t_minSize, t_maxSize, t_pos, t_vec) {
-  this.id = t_id;
-  t_minSize = t_minSize || 2;
-  t_maxSize = t_maxSize || 5;
-  this.size = Math.random() * Math.abs(t_maxSize - t_minSize) + Math.abs(t_minSize);
-  this.pos = t_pos || {x: 0, y: 0};
-  this.vec = t_vec || {x: 0, y: 0};
-}
-Node.prototype.distanceTo = function (t_nd) {
-  var x = t_nd.pos.x - this.pos.x;
-  var y = t_nd.pos.y - this.pos.y;
-  return Math.sqrt(x * x + y * y);
-};
-
 atc.Instance = atc.Instance || function (t_el, t_conf, t_id) {
   if (!(this instanceof atc.Instance)) return new atc.Instance(t_el, t_conf, t_id);
 
@@ -181,10 +167,9 @@ atc.Instance = atc.Instance || function (t_el, t_conf, t_id) {
     return pos;
   }
   function getRndVec () {
-    var vec = {};
-    vec.x = Math.random() * 2 * conf.maxSpeed - conf.maxSpeed;
-    vec.y = Math.random() * 2 * conf.maxSpeed - conf.maxSpeed;
-    return vec;
+    var x = Math.random() * 2 * conf.maxSpeed - conf.maxSpeed;
+    var y = Math.random() * 2 * conf.maxSpeed - conf.maxSpeed;
+    return new Vector(x, y);
   }
   function setMaxNodes (t_size) {
     if (t_size < maxNodes) { // need to delete
@@ -418,7 +403,6 @@ atc.Render = atc.Render || function (t_el, t_instance) {
 
   return {
     doRenderCycle: function () {
-      // var begin = window.performance.now();
       drawBackground();
       var nds = instance.getNodes();
       for (let i = 0; i < nds.length; ++i) {
@@ -432,8 +416,6 @@ atc.Render = atc.Render || function (t_el, t_instance) {
           }
         }
       }
-      // var deltaEdges = window.performance.now() - begin;
-      // var beginNodes = window.performance.now();
       for (let i = 0; i < nds.length; ++i) {
         drawNode(nds[i]);
       }
@@ -445,9 +427,6 @@ atc.Render = atc.Render || function (t_el, t_instance) {
           diagnostics = new atc.Diagnostics(instance);
         }
       }
-      // var deltaNodes = window.performance.now() - beginNodes;
-      // var delta = window.performance.now() - begin;
-      // console.log('Render took: ' + Math.round(delta, 2) + 'ms' + ' Edges: ' + Math.round(deltaEdges, 2) + 'ms  Nodes: ' + Math.round(deltaNodes, 2) + 'ms');
     }
   };
 };
@@ -455,19 +434,20 @@ atc.Render = atc.Render || function (t_el, t_instance) {
 atc.Physics = atc.Physics || function (t_instance) {
   var instance = t_instance;
 
-  function removeNodesOutOfBox () {
+  function bounceNodesFromWalls () {
+    var decay = 1.0; // 0.9;
     var nds = instance.getNodes();
     var dim = instance.getDimensions();
-    var rem = 0;
     for (let i = 0; i < nds.length; ++i) {
-      if (nds[i] === null) { console.log('nds null at: ' + i + ' nds.length = ' + nds.length); }
-      if (nds[i].pos.x < 0 || nds[i].pos.x > dim.x || nds[i].pos.y < 0 || nds[i].pos.y > dim.y) {
-        instance.removeNode(i);
-        ++rem;
+      var nd = nds[i];
+      if ((nd.pos.x - nd.size < 0 && nd.vec.x < 0) || (nd.pos.x + nd.size >= dim.x && nd.vec.x > 0)) { // switch X only if vector points outwards
+        nd.vec.x *= -1;
+        nd.vec.scaleBy(decay);
       }
-    }
-    for (let i = 0; i < rem; ++i) {
-      instance.addNode();
+      if ((nd.pos.y - nd.size < 0 && nd.vec.y < 0) || (nd.pos.y + nd.size >= dim.y && nd.vec.y > 0)) { // switch Y
+        nd.vec.y *= -1;
+        nd.vec.scaleBy(decay);
+      }
     }
   }
 
@@ -485,17 +465,139 @@ atc.Physics = atc.Physics || function (t_instance) {
     }
   }
 
+  function getSpeedFactor () {
+    return 1 / instance.getMaxFPS();
+  }
+
   function moveNodes () {
     var nds = instance.getNodes();
-    var speedFactor = 1 / instance.getMaxFPS();
+    var speedFactor = getSpeedFactor();
     for (let idx = 0; idx < nds.length; ++idx) {
-      nds[idx].pos.x += nds[idx].vec.x * speedFactor;
-      nds[idx].pos.y += nds[idx].vec.y * speedFactor;
+      var nd = nds[idx];
+      if (nd.physics.locked !== true) {
+          nd.pos.x += nd.vec.x * speedFactor;
+          nd.pos.y += nd.vec.y * speedFactor;
+        }
+      }
+    }
+  }
+
+  function applyCollsion (t_nd1, t_nd2) {
+    var radDist = t_nd1.size + t_nd2.size;
+    var cnt = 0;
+    while (radDist > t_nd1.distanceTo(t_nd2)) { // Move back to point of actual collision
+      t_nd1.pos.x += -t_nd1.vec.x * getSpeedFactor() * 0.1;
+      t_nd1.pos.y += -t_nd1.vec.y * getSpeedFactor() * 0.1;
+      t_nd2.pos.x += -t_nd2.vec.x * getSpeedFactor() * 0.1;
+      t_nd2.pos.y += -t_nd2.vec.y * getSpeedFactor() * 0.1;
+      ++cnt;
+    }
+    // console.log('Took ' + cnt + ' steps back');
+    var calcVsAndVe = function (nd1, nd2) { // vS in Richtung der Stoßnormalen, vE Orthogonal
+      var n = Vector.fromNodes(nd1, nd2);
+      n.scaleTo(nd1.size);
+      var alph = nd1.vec.angle(n);
+      var vS = new Vector(n);
+      vS.scaleTo(nd1.vec.length() * Math.cos(alph));
+      var vE = new Vector();
+      vE.x = nd1.vec.x - vS.x;
+      vE.y = nd1.vec.y - vS.x / n.x * n.y;
+      return { vS: vS, vE: vE };
+    };
+    var obj1 = calcVsAndVe(t_nd1, t_nd2);
+    var vS1 = obj1.vS; var vE1 = obj1.vE;
+    var obj2 = calcVsAndVe(t_nd2, t_nd1);
+    var vS2 = obj2.vS; var vE2 = obj2.vE;
+
+    var calcU = function (v1, v2, m1, m2) { // Resultierender Vektor parallel zu Stoßnormalen
+      let x = (m1 * v1.x + m2 * (2 * v2.x - v1.x)) / (m1 + m2);
+      let y = (m1 * v1.y + m2 * (2 * v2.y - v1.y)) / (m1 + m2);
+      return new Vector(x, y);
+    };
+    var uS1 = calcU(vS1, vS2, t_nd1.mass, t_nd2.mass);
+    var uS2 = calcU(vS2, vS1, t_nd2.mass, t_nd1.mass);
+    uS1.add(vE1);
+    uS2.add(vE2);
+    t_nd1.vec = uS1;
+    t_nd2.vec = uS2;
+  }
+
+  function checkCollisions () {
+    var nds = instance.getNodes();
+    var dim = instance.getDimensions();
+    var quad = new QuadTree(0, new Rectangle(0, 0, dim.x, dim.y));
+    quad.addItems(nds);
+    for (let i = 0; i < nds.length; ++i) {
+      var nd = nds[i];
+      var colNds = quad.queryItems(nd.pos.x, nd.pos.y);
+      for (let j = 0; j < colNds.length; ++j) {
+        if (colNds[j].id > i) {
+          if (nd.collidesWith(colNds[j])) {
+            // Check if they are moving away from each other
+            applyCollsion(nd, colNds[j]);
+          }
+        }
+      }
+    }
+  }
+
+  function removeNodesOutOfBox () {
+    var nds = instance.getNodes();
+    var dim = instance.getDimensions();
+    var rem = 0;
+    var minDist = 50;
+    for (let i = 0; i < nds.length; ++i) {
+      if (nds[i] === null) { console.log('nds null at: ' + i + ' nds.length = ' + nds.length); }
+      if (nds[i].pos.x < 0 - minDist || nds[i].pos.x > dim.x + minDist || nds[i].pos.y < 0 - minDist || nds[i].pos.y > dim.y + minDist) {
+        instance.removeNode(i);
+        ++rem;
+      }
+    }
+    for (let i = 0; i < rem; ++i) {
+      instance.addNode();
+    }
+  }
+
+  function electricalForce (t_nd1, t_nd2, t_dist) {
+    t_dist = t_dist || t_nd1.distanceTo(t_nd2);
+    var k = 10000;
+    return k * t_nd1.charge * t_nd2.charge / Math.pow(t_dist, 2);
+  }
+
+  function tickElectricalForce () {
+    var step = 1000 / instance.getMaxFPS();
+    var nds = instance.getNodes();
+    for (let i = 0; i < nds.length - 1; ++i) {
+      var nd1 = nds[i];
+      for (let j = i + 1; j < nds.length; ++j) {
+        var nd2 = nds[j];
+        var distance = nd1.distanceTo(nd2);
+        if (distance < 400 && distance > 10) {
+          // Take force and accelerate the nodes in direction of vector times the physics time
+          var f = electricalForce(nd1, nd2, distance);
+          var v = Vector.fromNodes(nd1, nd2);
+          var a = f / nd1.mass;
+          v.invert();
+          v.scaleTo(a * step);
+          nd1.vec.x += v.x;
+          nd1.vec.y += v.y;
+          // console.log('Vector changed by x:' + v.x + '  y:' + v.y);
+          // Repeat steps for nd2
+          a = f / nd2.mass;
+          v.invert();
+          v.scaleTo(a * step);
+          nd2.vec.x += v.x;
+          nd2.vec.y += v.y;
+        }
+      }
     }
   }
 
   return {
     tick: function () {
+      tickElectricalForce();
+      bounceNodesFromWalls();
+      checkCollisions();
       moveNodes();
       removeNodesOutOfBox();
       checkEdges();
@@ -508,6 +610,10 @@ atc.UserInterface = atc.UserInterface || function (t_el, t_instance) {
   var mouseDown = false;
   var focusedNode = null;
   var el = t_el;
+
+  var mouseLastX = null;
+  var mouseLastY = null;
+  var mouseLastStamp = null;
 
   function findNode (t_x, t_y) {
     var delta = 5;
@@ -527,10 +633,11 @@ atc.UserInterface = atc.UserInterface || function (t_el, t_instance) {
     mouseDown = true;
     var nd = findNode(event.layerX, event.layerY);
     if (nd !== null) {
-      nd.vecBak = nd.vec;
-      nd.vec = {x: 0, y: 0};
+      nd.vec = new Vector();
+      nd.physics.locked = true;
       focusedNode = nd;
       el.style.cursor = 'move';
+      setLastMousePosition(event);
     }
   }
 
@@ -538,6 +645,7 @@ atc.UserInterface = atc.UserInterface || function (t_el, t_instance) {
     if (mouseDown && focusedNode !== null) {
       focusedNode.pos.x = event.layerX;
       focusedNode.pos.y = event.layerY;
+      setLastMousePosition(event);
     } else {
       if (findNode(event.layerX, event.layerY) !== null) { // Takes around 0.5 ms
         el.style.cursor = 'pointer';
@@ -550,13 +658,34 @@ atc.UserInterface = atc.UserInterface || function (t_el, t_instance) {
   function onMouseUp (event) {
     mouseDown = false;
     if (focusedNode !== null) {
-      focusedNode.vec = focusedNode.vecBak;
-      delete focusedNode.vecBak;
+      if (mouseLastX !== null && mouseLastY !== null && mouseLastStamp !== null) {
+        var v = new Vector(event.layerX - mouseLastX, event.layerY - mouseLastY);
+        v.scaleBy(1000 / (window.performance.now() - mouseLastStamp));
+        focusedNode.vec = v;
+      } else {
+        focusedNode.vec = new Vector();
+      }
+      focusedNode.physics.locked = false;
       focusedNode = null;
     }
+    resetLastMousePosition();
     el.style.cursor = 'default';
   }
 
+  function setLastMousePosition (t_event) {
+    if (t_event !== undefined &&
+      (mouseLastStamp === null || mouseLastStamp < (window.performance.now() - 100))
+    ) {
+      mouseLastX = t_event.layerX;
+      mouseLastY = t_event.layerY;
+      mouseLastStamp = window.performance.now() | 0;
+    }
+  }
+  function resetLastMousePosition () {
+    mouseLastX = null;
+    mouseLastY = null;
+    mouseLastStamp = null;
+  }
   return {
     bind: function () {
       el.addEventListener('mousedown', onMouseDown);
@@ -590,30 +719,6 @@ atc.Diagnostics = atc.Diagnostics || function (t_instance) {
     }
     return d.getTime();
   }
-  function HSVtoRGB (t_H, t_S, t_V) { // t_H [0, 360], t_S, t_V [0,1]
-    var h = Math.round(t_H / 60);
-    var f = t_H / 60 - h;
-    var p = t_V * (1 - t_S);
-    var q = t_V * (1 - t_S * f);
-    var t = t_V * (1 - t_S * (1 - f));
-    var rgb = {};
-    switch (h) {
-      case 1: rgb = {r: q, g: t_V, b: p}; break;
-      case 2: rgb = {r: p, g: t_V, b: t}; break;
-      case 3: rgb = {r: p, g: q, b: t_V}; break;
-      case 4: rgb = {r: t, g: p, b: t_V}; break;
-      case 5: rgb = {r: t_V, g: p, b: q}; break;
-      default:rgb = {r: t_V, g: t, b: p}; break;
-    }
-    rgb.r = Math.round(rgb.r * 255);
-    rgb.g = Math.round(rgb.g * 255);
-    rgb.b = Math.round(rgb.b * 255);
-    return rgb;
-  }
-
-  function RGBtoString (t_rgb) {
-    return 'rgb(' + t_rgb.r + ',' + t_rgb.g + ',' + t_rgb.b + ')';
-  }
 
   return {
     tick: function () {
@@ -635,17 +740,232 @@ atc.Diagnostics = atc.Diagnostics || function (t_instance) {
         var j = (idx - 1) - i; // Dont render current idx when starting at i = 0
         if (j < 0) j = frames.length + j;
         var fps = frames[j] * resolution;
-        t_ctx.fillStyle = RGBtoString(HSVtoRGB(120 * fps / maxFPS, 1, 1));
+        let col = Color.fromHSV(120 * fps / maxFPS, 1, 1);
+        t_ctx.fillStyle = col.toRGBString();
         t_ctx.fillRect(t_x + x - i * 2, t_y + 25, -2, -20 * fps / maxFPS);
       }
       var lastFps = frames[idx === 0 ? frames.length - 1 : idx - 1];
       if (lastFps === undefined) lastFps = 0;
       lastFps *= resolution;
       t_ctx.font = '9x Arial';
-      t_ctx.fillStyle = RGBtoString(HSVtoRGB(120 * lastFps / maxFPS, 1, 1));
+      var col = Color.fromHSV(120 * lastFps / maxFPS, 1, 1);
+      t_ctx.fillStyle = col.toRGBString();
       t_ctx.fillText(lastFps + ' fps', t_x + x + 5, t_y + 12);
       t_ctx.fillStyle = 'white';
       t_ctx.fillText(maxFPS + ' fps', t_x + x + 5, t_y + 24);
     }
   };
+};
+
+function Color (r, g, b) {
+  this.r = r;
+  this.g = g;
+  this.b = b;
+}
+Color.fromHSV = function (t_H, t_S, t_V) {
+  var h = Math.round(t_H / 60);
+  var f = t_H / 60 - h;
+  var p = t_V * (1 - t_S);
+  var q = t_V * (1 - t_S * f);
+  var t = t_V * (1 - t_S * (1 - f));
+  var rgb = {};
+  switch (h) {
+    case 1: rgb = {r: q, g: t_V, b: p}; break;
+    case 2: rgb = {r: p, g: t_V, b: t}; break;
+    case 3: rgb = {r: p, g: q, b: t_V}; break;
+    case 4: rgb = {r: t, g: p, b: t_V}; break;
+    case 5: rgb = {r: t_V, g: p, b: q}; break;
+    default:rgb = {r: t_V, g: t, b: p}; break;
+  }
+  rgb.r = Math.round(rgb.r * 255);
+  rgb.g = Math.round(rgb.g * 255);
+  rgb.b = Math.round(rgb.b * 255);
+  return new Color(rgb.r, rgb.g, rgb.b);
+};
+Color.prototype.toRGBString = function () {
+  return 'rgb(' + this.r + ',' + this.g + ',' + this.b + ')';
+};
+
+function Vector (t_x, t_y) {
+  if (t_x instanceof Vector && t_y === undefined) {
+    this.x = t_x.x;
+    this.y = t_x.y;
+  } else {
+    this.x = t_x || 0;
+    this.y = t_y || 0;
+  }
+}
+Vector.copy = function (t_v) {
+  return new Vector(t_v.x, t_v.y);
+};
+Vector.fromNodes = function (t_nd1, t_nd2) {
+  return new Vector(t_nd2.pos.x - t_nd1.pos.x, t_nd2.pos.y - t_nd1.pos.y);
+};
+Vector.prototype.add = function (t_v) {
+  this.x += t_v.x;
+  this.y += t_v.y;
+};
+Vector.prototype.angle = function (t_v) {
+  var sc = this.scalar(t_v);
+  var l = this.length() * t_v.length();
+  if (l === 0) return 0;
+  var c = sc / l;
+  if (c < -1) c = -1;
+  if (c > 1) c = 1;
+  return Math.acos(c);
+};
+Vector.prototype.invert = function () {
+  this.x *= -1;
+  this.y *= -1;
+};
+Vector.prototype.length = function () {
+  return Math.sqrt(this.x * this.x + this.y * this.y);
+};
+Vector.prototype.scalar = function (t_v) {
+  return this.x * t_v.x + this.y * t_v.y;
+};
+Vector.prototype.scaleBy = function (t_scale) {
+  this.x *= t_scale;
+  this.y *= t_scale;
+};
+Vector.prototype.scaleTo = function (t_length) {
+  var len = this.length();
+  if (len !== 0) {
+    var x = t_length / len;
+    this.x *= x;
+    this.y *= x;
+  }
+};
+
+function Node (t_id, t_minSize, t_maxSize, t_pos, t_vec) {
+  this.id = t_id;
+  t_minSize = t_minSize || 2;
+  t_maxSize = t_maxSize || 5;
+  this.size = Math.random() * Math.abs(t_maxSize - t_minSize) + Math.abs(t_minSize);
+  this.pos = t_pos || {x: 0, y: 0};
+  this.vec = t_vec || new Vector();
+  // this.mass = 1;
+  this.mass = (4 / 3) * Math.PI * Math.pow(this.size, 3);
+  this.charge = 1;// Math.random() < 0.5 ? -1 : 1;
+  // this.charge = 1;
+  this.physics = { locked: false, skip: false };
+}
+Node.prototype.distanceTo = function (t_nd) {
+  var x = t_nd.pos.x - this.pos.x;
+  var y = t_nd.pos.y - this.pos.y;
+  return Math.sqrt(x * x + y * y);
+};
+Node.prototype.collidesWith = function (t_nd) {
+  if (this.id === t_nd.id) return false;
+  return this.distanceTo(t_nd) < (this.size + t_nd.size);
+};
+
+function Rectangle (t_x1, t_y1, t_x2, t_y2) {
+  if (t_x1 > t_x2) { let tmp = t_x1; t_x1 = t_x2; t_x2 = tmp; }
+  if (t_y1 > t_y2) { let tmp = t_y1; t_y1 = t_y2; t_y2 = tmp; }
+  this.x1 = t_x1;
+  this.x2 = t_x2;
+  this.y1 = t_y1;
+  this.y2 = t_y2;
+  this.xmid = Math.floor((t_x2 - t_x1) / 2) + t_x1;
+  this.ymid = Math.floor((t_y2 - t_y1) / 2) + t_y1;
+}
+
+function QuadTree (t_lvl, t_rect) {
+  this.bounds = t_rect;
+  this.lvl = t_lvl;
+  this.maxObjects = 10; // We will have to change and observe the behaviour
+  this.maxLvl = 5;
+  this.objects = [];
+  this.spares = []; // Objects that collide with the mid axes
+  this.nodes = [];
+  this.isSplit = false;
+}
+QuadTree.prototype.addItems = function (t_arr) {
+  for (let i in t_arr) {
+    this.addItem(t_arr[i]);
+  }
+};
+QuadTree.prototype.split = function () {
+  if (!this.isSplit) {
+    this.nodes = Array(4);
+    this.nodes[0] = new QuadTree(this.lvl + 1, new Rectangle(0, 0, this.bounds.xmid, this.bounds.ymid)); // TopLeft
+    this.nodes[1] = new QuadTree(this.lvl + 1, new Rectangle(this.bounds.xmid, 0, this.bounds.x2, this.bounds.ymid)); // Top right
+    this.nodes[2] = new QuadTree(this.lvl + 1, new Rectangle(this.bounds.xmid, this.bounds.ymid, this.bounds.x2, this.bounds.y2)); // Bottom right
+    this.nodes[3] = new QuadTree(this.lvl + 1, new Rectangle(0, this.bounds.ymid, this.bounds.xmid, this.bounds.y2)); // Bottom left
+    this.isSplit = true;
+    // Move Objects in their Sectors
+    for (let i = 0; i < this.objects.length; ++i) {
+      var obj = this.objects[i];
+      if (this.isSpare(obj)) {
+        this.spares.push(obj);
+      } else {
+        var s = this.getSector(obj.pos.x, obj.pos.y);
+        this.nodes[s].addItem(obj);
+      }
+    }
+    this.objects = null;
+  }
+};
+QuadTree.prototype.isSpare = function (t_nd) {
+  var fac = 3;
+  if (t_nd.pos.x >= this.bounds.midx - t_nd.size * fac && t_nd.pos.x <= this.bounds.midx + t_nd.size * fac) {
+    return true;
+  }
+  if (t_nd.pos.y >= this.bounds.midy - t_nd.size * fac && t_nd.pos.y <= this.bounds.midy + t_nd.size * fac) {
+    return true;
+  }
+  return false;
+};
+QuadTree.prototype.getSector = function (t_x, t_y) {
+  /* Sectors are placed this way:
+  1 | 2
+  ---|---
+  4 | 3
+  */
+  var xmid = Math.floor((this.bounds.x2 - this.bounds.x1) / 2) + this.bounds.x1;
+  var ymid = Math.floor((this.bounds.y2 - this.bounds.y1) / 2) + this.bounds.y1;
+  if (t_x > this.bounds.x1 && t_x < xmid) { // Sector 1 or 4
+    if (t_y > this.bounds.y1 && t_y < ymid) {
+      return 0; // Sector 1
+    } else {
+      return 3; // Sector 4
+    }
+  } else {
+    if (t_x > this.bounds.y1 && t_y < ymid) {
+      return 1; // Sector 2
+    } else {
+      return 2; // Sector 3
+    }
+  }
+};
+QuadTree.prototype.addItem = function (t_item) {
+  if (!this.isSplit) {
+    if (this.objects.length + 1 > this.maxObjects && this.lvl <= this.maxLvl) {
+      this.split();
+      this.addItem(t_item);
+    } else {
+      this.objects.push(t_item);
+    }
+  } else {
+    // Get QuadTree
+    if (!this.isSpare(t_item)) {
+      var s = this.getSector(t_item.pos.x, t_item.pos.y);
+      this.nodes[s].addItem(t_item);
+    } else {
+      this.spares.push(t_item);
+    }
+  }
+};
+QuadTree.prototype.queryItems = function (t_x, t_y) {
+  var arr = [];
+  arr = arr.concat(this.spares);
+  if (!this.isSplit) {
+    arr = arr.concat(this.objects);
+  } else {
+    var s = this.getSector(t_x, t_y);
+    var res = this.nodes[s].queryItems(t_x, t_y);
+    arr = arr.concat(res);
+  }
+  return arr;
 };
